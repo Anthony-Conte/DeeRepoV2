b602e3ec1388a1afd58125bd5fe34c5352fdbb00
Anthony <conteanthony@ymail.com>
2026-07-14 15:50:26 -0500

    fix entity

--- apps/dee-list-api/src/app/item/entities/item.entity.ts
+++ apps/dee-list-api/src/app/item/entities/item.entity.ts
@@ -2,26 +2,19 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  JoinColumn,
-  ManyToOne,
   PrimaryGeneratedColumn,
-  RelationId,
 } from 'typeorm';
 @Entity({ name: 'items' })
 export class Item {
   @PrimaryGeneratedColumn('uuid')
-  public id: string;
+  public id!: string;
 
   @Column({ length: 255 })
-  public name: string;
+  public name!: string;
 
-  //   @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
-  //   @JoinColumn({ name: 'created_by_user_id' })
-  //   public createdBy: User;
-
-  @RelationId((item: ListItem) => item.createdBy)
-  public createdByUserId: string;
+  @Column({ type: 'uuid', name: 'created_by_user_id' })
+  public createdByUserId!: string;
 
   @CreateDateColumn({ name: 'created_at' })
-  public createdAt: Date;
+  public createdAt!: Date;
 }
