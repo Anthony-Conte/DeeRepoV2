01b37d9949154c144616b78812ba36491687a534
Anthony <conteanthony@ymail.com>
2026-07-14 10:29:35 -0500

    item DTO WIP

--- apps/dee-list-api/src/app/item/dto/create-item.dto.ts
+++ apps/dee-list-api/src/app/item/dto/create-item.dto.ts
@@ -1,1 +1,13 @@
-export class CreateItemDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MaxLength } from 'class-validator';
+
+export class CreateItemDto {
+  @ApiProperty({
+    description: 'The name of the list item.',
+    example: 'Watermelon',
+    maxLength: 255,
+  })
+  @IsString()
+  @MaxLength(255)
+  public name = '';
+}
--- apps/dee-list-api/src/app/item/entities/item.entity.ts
+++ apps/dee-list-api/src/app/item/entities/item.entity.ts
@@ -1,1 +1,27 @@
-export class Item {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  RelationId,
+} from 'typeorm';
+@Entity({ name: 'items' })
+export class Item {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column({ length: 255 })
+  public name: string;
+
+  //   @ManyToOne(() => User, { nullable: false, onDelete: 'CASCADE' })
+  //   @JoinColumn({ name: 'created_by_user_id' })
+  //   public createdBy: User;
+
+  @RelationId((item: ListItem) => item.createdBy)
+  public createdByUserId: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  public createdAt: Date;
+}
