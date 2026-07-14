e0750a77d69eb0268ce5349322429bb29e130c62
Anthony <conteanthony@ymail.com>
2026-07-13 23:00:36 -0500

    add DB config

--- apps/dee-list-api/src/app/app.module.ts
+++ apps/dee-list-api/src/app/app.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemModule } from './item/item.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ItemModule],
+  imports: [
+    ItemModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: false,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
