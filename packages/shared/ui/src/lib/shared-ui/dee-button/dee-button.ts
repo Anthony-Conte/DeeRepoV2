d1e0a46cb5862663f8ed88a0208cfa6855d7e057
Anthony <conteanthony@ymail.com>
2026-07-19 10:57:13 -0500

    button styles

--- packages/shared/ui/src/lib/shared-ui/dee-button/dee-button.ts
+++ packages/shared/ui/src/lib/shared-ui/dee-button/dee-button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-dee-button',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './dee-button.html',
   styleUrl: './dee-button.css'
 })
-export class DeeButton {}
+export class DeeButton {
+  @Input() public variant: 'primary' | 'secondary' | 'error' = 'primary';
+}
