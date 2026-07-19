9c6fce63f9fd6f16a2c7547927a70f68c02d440c
Anthony <conteanthony@ymail.com>
2026-07-19 16:36:11 -0500

    setup swagger

--- apps/auth-api/src/main.ts
+++ apps/auth-api/src/main.ts
@@ -6,15 +6,35 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('Auth backend API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
+
   const port = process.env.PORT || 3001;
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true
+  });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+
+  Logger.log(
+    `📚 Swagger documentation available at: http://localhost:${port}/${globalPrefix}/docs`
   );
 }
 
